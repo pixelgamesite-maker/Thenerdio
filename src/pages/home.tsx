dd9781de2543511be4d723be79848fb891e3ffdf
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-07-15 20:45:18 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,10 @@
 import { LoreSection } from "@/components/nerdio/LoreSection";
 import { FaqSection } from "@/components/nerdio/FaqSection";
-import { rule } from "@/lib/theme";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "28px" }}>
       <LoreSection />
-      <div style={{ height: "1px", background: rule, margin: "36px 0 30px" }} />
       <FaqSection />
     </div>
   );
